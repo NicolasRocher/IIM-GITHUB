40058cedf746d893a07fd1106bbc0870b6cc89ff
Rémi VIGNEUX <Rmi VIGNEUX>
2016-09-14 14:10:51 +0200

    Button -valid- -> profil section

--- view/edit_image.php
+++ view/edit_image.php
@@ -4,7 +4,7 @@
 		<div class="row">
 			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 				<div id="musicfeed">
-					<h1><i class="fa fa-pencil"></i> Editer votre image de profil</h1>
+					<h1><i class="fa fa-pencil"></i> Votre profil SoundCloud</h1>
 					<div class="block animated fadeInDown">
 						<div class="row">
 							<div class="col-xs-2 col-sm-2 col-md-1 col-lg-1">
