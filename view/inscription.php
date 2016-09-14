8d138d9aa7f50aac30a976d7b9af159071a829f7
NicolasRocher <rochernicolas97@orange.fr>
2016-09-14 18:10:15 +0200

    Register type password

--- view/inscription.php
+++ view/inscription.php
@@ -6,7 +6,7 @@
 <form  method="post" action="inscription.php">
 <input type="text" name="nom" placeholder="Nom" />
 <input type="email" name="email" placeholder="Email" />
-<input type="text" name="mdp" placeholder="Mot de passe" />
+<input type="password" name="mdp" placeholder="Mot de passe" />
 <input type="submit" value="S'inscrire" />
 </form>
 </div>
