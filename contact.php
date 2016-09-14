2620eed43cffc1e01ab07dcf781b7cad30b6846c
NicolasRocher <rochernicolas97@orange.fr>
2016-09-14 16:40:40 +0200

    contact form

--- contact.php
+++ contact.php
@@ -9,6 +9,24 @@ require('model/functions.fn.php');
 include 'view/_header.php';
 include 'view/contact.php';
 include 'view/_footer.php';
+ include('config.php');
+
+
+
+$nom = $_POST['nom'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+
+
+
+
+$sql="INSERT INTO contact (nom, email, message)
+VALUES ('$nom','$email','$message')";
+
+
+$req = $db->prepare($sql);
+$req->execute(); ?>
 
 
 	
--- view/contact.php
+++ view/contact.php
@@ -1,3 +1,15 @@
 <body>
 <?php include '_topbar.php'; ?>
+
+
+<div class="form-style-6">
+<h2>Contactez-nous</h2>
+<form  method="post" action="addcontact.php">
+<input type="text" name="nom" placeholder="Nom" />
+<input type="email" name="email" placeholder="Email" />
+<textarea name="message" placeholder="Votre message"></textarea>
+<input type="submit" value="Envoyer" />
+</form>
+</div>
+
 </body>
