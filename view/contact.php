5cb8abc683c85e5e501f1f9f9bef55385c92585f
Rémi VIGNEUX <Rmi VIGNEUX>
2016-09-16 11:09:48 +0200

    Contact style

--- view/contact.php
+++ view/contact.php
@@ -1,21 +1,31 @@
 <body>
 <?php include '_topbar.php'; ?>
 
-
-<div class="row">
+<div class="container-fluid">
     <div class="col-md-2 col-sm-2 col-xs-0"></div>
-    <div class="col-md-8 col-sm-8 col-xs-12">
-        <h4>Envoyez-nous un message !</h4>
-        <form method="post" action="add_message.php">
-            <label for="email">Email :</label><br>
-            <input required type="email" name="email"><br>
-            <label for="message">Votre message :</label><br>
-            <textarea id="message" rows="5" cols="5" name="text"></textarea><br>
-            <button type="submit" id="sendmess">Envoyer</button>
-            <button type="reset" id="resetmess">Annuler</button>
-        </form>
+    <div class="divcontact col-md-8 col-sm-8 col-xs-12">
+        <div class="row">
+            <div class="contact col-md-12 col-sm-12 col-xs-12">
+                <h4>Envoyez-nous un message !</h4>
+                <div class="row">
+                    <div class="col-md-4"></div>
+                    <div class="col-md-4">
+                        <form method="post" action="add_message.php">
+                            <input required type="email" name="email" placeholder="Votre @" id="sendmail"><br>
+                            <textarea id="message" rows="5" cols="30" name="text" placeholder="Votre message :" ></textarea><br>
+                            <button type="submit" id="sendmess">Envoyer</button>
+                            <button type="reset" id="resetmess">Annuler</button>
+                        </form>
+                    </div>
+                    <div class="col-md-4"></div>
+                </div>
+
+            </div>
+        </div>
     </div>
     <div class="col-md-2 col-sm-2 col-xs-0"></div>
+
 </div>
+
 
 </body>
