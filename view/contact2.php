67a65bc8bfaa1bced0be3e3252fa80c16793841a
Rémi VIGNEUX <Rmi VIGNEUX>
2016-09-16 10:01:04 +0200

    New contact

---
+++ view/contact2.php
@@ -0,0 +1,9 @@
+<body>
+    <div class="row">
+        <div class="col-md-2 col-sm-2 col-xs-0"></div>
+        <div class="col-md-8 col-sm-8 col-xs-12">
+            
+        </div>
+        <div class="col-md-2 col-sm-2 col-xs-0"></div>
+    </div>
+</body>
